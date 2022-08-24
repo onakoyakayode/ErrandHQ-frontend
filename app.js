adc10d87d30fb6c44802bbb44e7767223e9f9f6e
Onakoya Kayode <onakoyakayode@gmail.com>
2022-08-24 18:53:33 +0100

    remove slider testimonial

--- app.js
+++ app.js
@@ -8,38 +8,9 @@ toggleBtn.addEventListener('click', (e) => {
   } else {
     navBar.style.left = "-120%"
   }
-})
+});
 
 
 
 
-var currentSlide = 0;
-var slides = document.querySelectorAll("#slides .slide");
-var controls = document.querySelectorAll(".controls");
-var next = document.getElementById("next");
-var previous = document.getElementById("previous");
 
-for (var i = 0; i < controls.length; i++) {
-    controls[i].style.display = "inline-block";
-}
-
-function goToSlide(n) {
-    slides[currentSlide].className = "slide";
-    currentSlide = (n + slides.length) % slides.length;
-    slides[currentSlide].className = "slide showing";
-}
-
-function nextSlide() {
-    goToSlide(currentSlide + 1);
-}
-
-function previousSlide() {
-    goToSlide(currentSlide - 1);
-}
-
-next.onclick = function() {
-    nextSlide();
-};
-previous.onclick = function() {
-    previousSlide();
-};
